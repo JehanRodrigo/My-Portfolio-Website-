52e5575d18a3b168ee78c1980b3b1221f80c33f5
JehanRodrigo <jehanrodrigo31@gmail.com>
2024-07-31 21:20:29 +0530

    Email Card edited

--- Data/index.ts
+++ Data/index.ts
@@ -63,7 +63,7 @@ export const gridItems = [
   },
   {
     id: 6,
-    title: "Do you want to start a project together?",
+    title: "Reach out to me via email.",
     description: "",
     className: "lg:col-span-2 md:col-span-3 md:row-span-1",
     imgClassName: "",
