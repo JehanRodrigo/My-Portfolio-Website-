5c1604abaee650731df21ec709a5b9b9b0bcca4a
JehanRodrigo <jehanrodrigo31@gmail.com>
2024-08-02 17:26:34 +0530

    Update Globe.tsx

--- components/ui/Globe.tsx
+++ components/ui/Globe.tsx
@@ -5,6 +5,8 @@ import ThreeGlobe from "three-globe";
 import { useThree, Object3DNode, Canvas, extend } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import countries from "@/data/globe.json";
+import { features } from "@/data/globe.json";
+import coordinates from "@/data/globe.json";
 declare module "@react-three/fiber" {
   interface ThreeElements {
     threeGlobe: Object3DNode<ThreeGlobe, typeof ThreeGlobe>;
